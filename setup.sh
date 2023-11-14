08751d97c81e2776a30447c4a9d5351ddf7f50f5
immrn <das.korn.flakes@gmail.com>
2023-11-14 11:52:24 +0100

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,11 @@ apt install git
 apt install python3.11
 apt install python3.11-venv
 apt install screen
+
+python3.11 -m venv venv
+. ./venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+
+echo "Finished setup!"
+echo "Add the email pw as file ..."
